b4e7dca8f68c58dddafe322ef77016cb0ec0362e
Adam Bailey <nadehz@Adams-MacBook-Pro-2.local>
2014-12-27 13:39:55 -0500

    Initial Commit

---
+++ SwiftSoundManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSoundManager
+//
+//  Created by Adam Bailey on 12/27/14.
+//  Copyright (c) 2014 Adam Bailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
